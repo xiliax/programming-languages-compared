1fda3e956aeae71afdbb4faf4674bf3e609ae5bf
Verrol Adams <another@xiliax.com>
2017-07-17 10:03:08 -0400

    c02s02 - C++ and Java

---
+++ 02-hello-world/c02s02/java/demo/Hello.java
@@ -0,0 +1,12 @@
+package demo;
+
+class Hello {
+    // method to say hello call sayHello()
+    static void sayHello() {
+        System.out.println("Hello, World");
+    }
+
+    public static void main(String[] args) {
+        Hello.sayHello();
+    }
+}
