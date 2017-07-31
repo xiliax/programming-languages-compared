041602b8891c9a898d3b32e549312bcad63520f3
Verrol Adams <another@xiliax.com>
2017-07-30 20:53:34 -0400

    c03s02 - C++ - Basic Data Types & Variables

--- 03-basic-types-and-variables/c03s01/c/main.c
+++ 03-basic-types-and-variables/c03s01/c/main.c
@@ -20,19 +20,14 @@ void varInit()
     double d = 90929324909023.290932342;
     long double ld = 90992343e208;
     unsigned short us = 65000;
-    unsigned char uc = 330;
-    printf("1.\t%c - %i\n", c, c);
-    printf("2.\t%c - %i\n", uc, uc);
-    printf("3.\t%i - %d\n", i, l);
-    printf("4.\t0x%x - ox%X\n", c, c);
-    printf("5.\t0x%x - 0x%X\n", uc, uc);
-    printf("6.\t0x%x - 0x%X\n", i, l);
-    printf("7.\t%f - %F\n", f, f);
-    printf("8.\t%f - %F\n", d, d);
-    printf("9.\t%e - %E\n", f, f);
-    printf("10.\t%e - %E\n", d, d);
-    printf("11.\t%g - %G\n", f, f);
-    printf("12.\t%g - %G\n", d, d);
+    unsigned char uc = 128;
+    printf("1.\tc - %c\n", c);
+    printf("2.\tc - %i\n", c);
+    printf("3.\ti - %d\n", i);
+    printf("4.\tl- 0x%lX\n", l);
+    printf("5.\tf - %F\n", f);
+    printf("6.\td - %E\n", d);
+    printf("7.\tld - %LG\n", ld);
 }
 
 void varDeclar()
@@ -46,16 +41,11 @@ void varDeclar()
     long double ld;
     unsigned short us;
     unsigned char uc;
-    printf("1.\t%c - %i\n", c, c);
-    printf("2.\t%c - %i\n", uc, uc);
-    printf("3.\t%i - %d\n", i, l);
-    printf("4.\t0x%x - ox%X\n", c, c);
-    printf("5.\t0x%x - 0x%X\n", uc, uc);
-    printf("6.\t0x%x - 0x%X\n", i, l);
-    printf("7.\t%f - %F\n", f, f);
-    printf("8.\t%f - %F\n", d, d);
-    printf("9.\t%e - %E\n", f, f);
-    printf("10.\t%e - %E\n", d, d);
-    printf("11.\t%g - %G\n", f, f);
-    printf("12.\t%g - %G\n", d, d);
+    printf("1.\tc - %c\n", c);
+    printf("2.\tc - %i\n", c);
+    printf("3.\ti - %d\n", i);
+    printf("4.\tl- 0x%lX\n", l);
+    printf("5.\tf - %F\n", f);
+    printf("6.\td - %E\n", d);
+    printf("7.\tld - %LG\n", ld);
 }
