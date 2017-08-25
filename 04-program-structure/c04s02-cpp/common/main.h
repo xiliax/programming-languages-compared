59e642039cf50149c398046b5a34bf5eab5118fc
Verrol Adams <another@xiliax.com>
2017-08-25 19:32:48 -0400

    c04s02 - C++ Application Structure

---
+++ 04-program-structure/c04s02-cpp/common/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <iostream>
+#include "ui/ui.h"
+#include "storage/storage.h"
+//#include "compute/compute.h"
+
+// ---- main stuff ----
+void get_user_input(std::auto_ptr<AA::AA_Ui> ui);
+
+#endif // __MAIN_H__
